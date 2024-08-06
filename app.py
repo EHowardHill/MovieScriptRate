da5cd5c0059c92b93ac8bcd2f21db16d2fa46768
Ethan Hill <ethan.hill1999@outlook.com>
2024-08-06 15:49:03 -0500

    added staging

--- app.py
+++ app.py
@@ -1,23 +1,22 @@
-import os
-from groq import Groq
-from selenium import webdriver
-
-from selenium.webdriver.chrome.service import Service as ChromeService
-from selenium.webdriver.chrome.options import Options as ChromeOptions
-
-from selenium.webdriver.firefox.service import Service as FirefoxService
-from selenium.webdriver.firefox.options import Options as FirefoxOptions
-from webdriver_manager.firefox import GeckoDriverManager
-
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait  # Import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC  # Import expected_conditions
-from webdriver_manager.chrome import ChromeDriverManager
-import markdownify
-from json import dumps, loads
-from time import sleep
-import openai
 from flask import Flask, render_template, request
+from groq import Groq
+from json import dumps, loads
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options as ChromeOptions
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.common.by import By
+from selenium.webdriver.firefox.options import Options as FirefoxOptions
+from selenium.webdriver.firefox.service import Service as FirefoxService
+from selenium.webdriver.support import expected_conditions as EC  # Import expected_conditions
+from selenium.webdriver.support.ui import WebDriverWait  # Import WebDriverWait
+from time import sleep
+from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.firefox import GeckoDriverManager
+import markdownify
+import openai
+import os
+from random import randint
+import threading
 
 # sudo apt install -y chromium-chromedriver
 
@@ -65,10 +64,10 @@ def query(message, llm_type="chatgpt"):
         )
         return chat_completion.choices[0].message.content
 
-def fetch_script(movie_name):
+def fetch_script(movie_name, log_file):
     global browser
 
-    if True: #try:
+    try:
         print("- loading IMSDB")
 
         driver = None
@@ -132,12 +131,18 @@ def fetch_script(movie_name):
         try:
             poster = driver.find_element(
                 By.XPATH, "//img[contains(@src, 'https://www.imsdb.com/posters')]"
-            ).get_attribute("src")
+            )
+            if (poster != None):
+                poster = poster.get_attribute("src")
+
         except Exception as e:
             print(str(e))
-            return None
+            with open(log_file, "w") as f:
+                f.write("Poster not found (but we're still searching)...")
 
-        name = driver.find_elements(By.TAG_NAME, "h1")[-1].text[:-7]
+        print("- Ignoring poster...")
+
+        film = driver.find_elements(By.TAG_NAME, "h1")[-1].text[:-7]
         read = driver.find_element(By.XPATH, "//a[contains(@href, '/scripts/')]").get_attribute(
             "href"
         )
@@ -152,156 +157,177 @@ def fetch_script(movie_name):
         print("- closing Selenium")
         h = markdownify.markdownify(pre, heading_style="ATX").replace("**", "")
         return {
-            "name": name,
+            "film": film,
             "poster": poster,
             "html": h
         }
-    #except Exception as e:
-    #    print(str(e))
-    #    return None
+    except Exception as e:
+        with open(log_file, "w") as f:
+            f.write("Error: " + str(e))
+        return None
 
-def rate(h):
+def new_job(movie_string, id, movie_path):
     try:
-        prior = "Does the above script portray"
-        final = "YES, NO, or MAYBE. Answer using the following JSON format: {'answer': 'YES', 'explanation': 'This is why', 'examples': 'When...'}"
+        print("Starting new job...")
 
-        questions = [
-            ["Traditional Gender Roles", "traditional gender roles, with men as protectors and provides, and women as nurturers and homemakers"],
-            ["Positive Masculinity", "positive depiction of male authority figures (fathers, husbands) as wise and benevolent leaders"],
-            ["Respectful Interactions Between Men & Women", "emphasis on the importance of harmonious relationships between men and women based on mutual respect and support"],
-            ["Respect for the Family Unit", "promotion of the nuclear family as the ideal social unit"],
-            ["Positive Fatherhood", "positive portrayal of fathers as authoritative figures guiding and mentoring their children"],
-            ["Respect of the Household", "depiction of the home as a place of security and refuge, with clear roles and responsibilities"],
-            ["Honor, Duty, & Responsibility", "emphasis on concepts like honor, duty, and responsibility, particularly from a male perspective"],
-            ["Modesty", "positive portrayal of modesty in dress and behavior, especially for women"],
-            ["Moral Courage", "highlighting masculine virtues such as physical strength, moral courage, and perseverance"],
-            ["Positive Male Leadership", "recognition and celebration of men in positions of leadership within communities, churches, and organizations"],
-            ["Acknowledgment of Male Sacrifice", "acknowledgment of sacrifices made by men for the betterment of their families and communities"],
-            ["Respect for Churches & Institutions", "positive depiction of institutions that uphold patriarchal values, such as churches and fraternal organizations"],
-            ["Respect for Cultural Values", "respect for traditional cultural practices and customs that uphold patriarchal norms"],
-            ["Historical Accuracy", "accuracy in depicting historical patriarchal societies and their contributions to culture and civilization"],
-            ["Exploration of Contemporary Challenges", "exploration of challenges to patriarchal values in modern society and their potential consequences"],
-        ]
+        log_file = "status/" + str(id)
+        with open(log_file, "w") as f:
+            f.write("Searching for the script online...")
 
-        data = {}
-        for question_pair in questions:
-            question_title = question_pair[0]
-            question = question_pair[1]
+        movie = fetch_script(movie_string, log_file)
+        if movie == None:
+            return
 
-            print(question)
-            response_json = None
-            attempts = 0
-            while response_json is None and attempts < 3:
-                attempts += 1
-                response = query(h + "\n" + prior + " " + question + "? " + final)
-                response_json = extract_json(response)
-                data[question_title] = response_json
-                print(response)
-            with open("final.json", "w") as f:
-                f.write(dumps(data))
-            #sleep(60) - activate this if you need to impliment token minute limits
-        
-        return data
+        try:
+            prior = "Does the above script portray"
+            final = "YES, NO, or MAYBE. Answer using the following JSON format: {'answer': 'YES', 'explanation': 'This is why', 'examples': 'When...'}"
+
+            questions = [
+                ["Traditional Gender Roles", "traditional gender roles, with men as protectors and provides, and women as nurturers and homemakers"],
+                ["Positive Masculinity", "positive depiction of male authority figures (fathers, husbands) as wise and benevolent leaders"],
+                ["Respectful Interactions Between Men & Women", "emphasis on the importance of harmonious relationships between men and women based on mutual respect and support"],
+                ["Respect for the Family Unit", "promotion of the nuclear family as the ideal social unit"],
+                ["Positive Fatherhood", "positive portrayal of fathers as authoritative figures guiding and mentoring their children"],
+                ["Respect of the Household", "depiction of the home as a place of security and refuge, with clear roles and responsibilities"],
+                ["Honor, Duty, & Responsibility", "emphasis on concepts like honor, duty, and responsibility, particularly from a male perspective"],
+                ["Modesty", "positive portrayal of modesty in dress and behavior, especially for women"],
+                ["Positive Male Leadership", "recognition and celebration of men in positions of leadership within communities, churches, and organizations"],
+                ["Acknowledgment of Male Sacrifice", "acknowledgment of sacrifices made by men for the betterment of their families and communities"],
+                ["Respect for Churches & Institutions", "positive depiction of institutions that uphold patriarchal values, such as churches and fraternal organizations"],
+                ["Respect for Cultural Values", "respect for traditional cultural practices and customs that uphold patriarchal norms"],
+                ["Historical Accuracy", "accuracy in depicting historical patriarchal societies and their contributions to culture and civilization"],
+                ["Exploration of Contemporary Challenges", "exploration of challenges to patriarchal values in modern society and their potential consequences"],
+            ]
+
+            questions_count = len(questions)
+
+            rating = {}
+            question_index = 0
+            for question_pair in questions:
+                question_index += 1
+
+                with open(log_file, "w") as f:
+                    f.write("Handling question " + str(question_index) + "/" + str(questions_count) + "...")
+
+                question_title = question_pair[0]
+                question = question_pair[1]
+
+                print(question)
+                response_json = None
+                attempts = 0
+                while response_json is None and attempts < 3:
+                    attempts += 1
+                    response = query(movie["html"] + "\n" + prior + " " + question + "? " + final)
+                    response_json = extract_json(response)
+                    rating[question_title] = response_json
+                    print(response)
+
+            # Cache answer for later
+            print("Saving...")
+            with open(movie_path, "w") as f:
+                f.write(dumps({"metadata": movie, "rating": rating, "success": 1}))
+
+        except Exception as e:
+            print(str(e))
+
     except Exception as e:
         print(str(e))
-        return None
+
+    os.remove(log_file)
+    return
 
 @app.route("/reelvalues", methods=["GET"])
 def main():
-    return render_template("index.html")
+    sample = [film.replace(".json", "") for film in os.listdir("movies")]
+    if len(sample) > 10:
+        sample = sample[:10]
+    return render_template("index.html", sample=sample)
 
 @app.route("/reelvalues", methods=["POST"])
 def fetch():
-    try:
-        movie_string = request.form.get("name")
-        movie_path = "movies/" + movie_string + ".json"
+    route = request.form.get("route")
+    movie_string = request.form.get("film")
+    movie_path = "movies/" + movie_string + ".json"
 
-        # If exists, just use that smh
-        if (os.path.exists(movie_path)):
-            with open(movie_path, "r") as f:
-                data = loads(f.read())
-                print(data)
+    if (route == "status"):
+        sleep(1)
 
-                count = 0
-                score = 0
-                for r in data["rating"]:
-                    print("R: " + r)
-
-                    if data["rating"][r] != None:
-                        count += 1
-                        if data["rating"][r]["answer"] == "YES":
-                            score += 1
-                        elif data["rating"][r]["answer"] == "MAYBE":
-                            score += 0.5
-
-                return {
-                    "name": data["metadata"]["name"],
-                    "poster": data["metadata"]["poster"],
-                    "rating": data["rating"],
-                    "score": int((score / count) * 100),
-                    "success": 1
-                }
-
-        movie = fetch_script(movie_string)
-
-        if movie == None:
-            return {"success": 0}
-
-        movie_path = "movies/" + movie["name"] + ".json"
-
-        # If exists, just use that smh
-        if (os.path.exists(movie_path)):
-            with open(movie_path, "r") as f:
-                data = loads(f.read())
-
-                count = 0
-                score = 0
-                for r in data["rating"]:
-                    if data["rating"][r] != None:
-                        count += 1
-                        if data["rating"][r]["answer"] == "YES":
-                            score += 1
-                        elif data["rating"][r]["answer"] == "MAYBE":
-                            score += 0.5
-
-                return {
-                    "name": data["metadata"]["name"],
-                    "poster": data["metadata"]["poster"],
-                    "rating": data["rating"],
-                    "score": int((score / count) * 100),
-                    "success": 1
-                }
-
-        rating = rate(movie["html"])
-        if rating == None:
+        path = "status/" + str(request.form.get("id"))
+        if os.path.exists(path):
+            message = ""
+            with open(path, "r") as f:
+                message = f.read()
             return {
-                "success": 2
+                "success": 3,
+                "message": message
             }
 
-        # Cache answer for later
-        data = {"metadata": movie, "rating": rating, "success": 1}
-        with open("movies/" + movie["name"] + ".json", "w") as f:
-            f.write(dumps(data))
+        else:
+            if (os.path.exists(movie_path)):
+                with open(movie_path, "r") as f:
+                    data = loads(f.read())
 
-        count = 0
-        score = 0
-        for r in data["rating"]:
-            if data["rating"][r] != None:
-                count += 1
-                if data["rating"][r]["answer"] == "YES":
-                    score += 1
-                elif data["rating"][r]["answer"] == "MAYBE":
-                    score += 0.5
+                    count = 0
+                    score = 0
+                    for r in data["rating"]:
+                        if data["rating"][r] != None:
+                            count += 1
+                            if data["rating"][r]["answer"] == "YES":
+                                score += 1
+                            elif data["rating"][r]["answer"] == "MAYBE":
+                                score += 0.5
 
-        return {
-            "name": movie["name"],
-            "poster": movie["poster"],
-            "rating": rating,
-            "score": int((score / count) * 100),
-            "success": 1
-        }
-    except Exception as e:
-        print(str(e))
-        return {
-            "success": 2
-        }
+                    return {
+                        "film": data["metadata"]["film"],
+                        "poster": data["metadata"]["poster"],
+                        "rating": data["rating"],
+                        "score": int((score / count) * 100),
+                        "success": 1
+                    }
+            
+            else:
+                return {
+                    "success": 2
+                }
+        
+    elif (route == "search"):
+
+        # If exists, just use that smh
+        if (os.path.exists(movie_path)):
+            with open(movie_path, "r") as f:
+                data = loads(f.read())
+
+                count = 0
+                score = 0
+                for r in data["rating"]:
+                    if data["rating"][r] != None:
+                        count += 1
+                        if data["rating"][r]["answer"] == "YES":
+                            score += 1
+                        elif data["rating"][r]["answer"] == "MAYBE":
+                            score += 0.5
+
+                return {
+                    "film": data["metadata"]["film"],
+                    "poster": data["metadata"]["poster"],
+                    "rating": data["rating"],
+                    "score": int((score / count) * 100),
+                    "success": 1
+                }
+            
+        else:
+
+            id = randint(0, 9999)
+            while os.path.exists("status/" + str(id)):
+                id = randint(0, 9999)
+
+            with open("status/" + str(id), "w") as f:
+                f.write("Starting new thread...")
+            
+            thread = threading.Thread(target=lambda: new_job(movie_string, id, movie_path))
+            thread.start()
+
+            return {
+                "success": 1,
+                "id": id
+            }
